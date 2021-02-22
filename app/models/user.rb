96eb3e9d90a59aa6778d9b08d5618531bb5ae76c
Brandon Lamb <blamb888@gmail.com>
2021-02-22 15:44:07 +0900

    added navbar and devise authentication gem

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  #   def configure_permitted_parameters
+  #   # For additional fields in app/views/devise/registrations/new.html.erb
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+  #   # For additional in app/views/devise/registrations/edit.html.erb
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  # end
+end
