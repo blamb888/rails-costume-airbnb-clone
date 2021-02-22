3b6713754475f1129e9b0fdf46cfe2b969f7da34
Brandon Lamb <blamb888@gmail.com>
2021-02-22 17:33:44 +0900

    added our seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Testing...."
+puts "Testing..."
+puts "Cleaning up db"
+Costume.destroy_all
+puts "Db is clean"
+
+20.times do |costume|
+  costume = Costume.create(
+    name: Faker::Superhero.name,
+    user: User.find(1),
+    category: Faker::Superhero.suffix,
+    description: Faker::Superhero.descriptor,
+    price: rand(20..99)
+  )
+puts costume.price
+puts "#{costume.name} was create!"
+end
+
+puts "#{Costume.count} costumes were created, homeslizzle."
