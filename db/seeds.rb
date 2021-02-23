bc60f5af3827648889bff7019161b752409c1207
Brandon Lamb <blamb888@gmail.com>
2021-02-23 16:58:54 +0900

    set up seeds with images and cloudinary services

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+require "open-uri"
 
 puts "Testing...."
 puts "Testing..."
 puts "Cleaning up db"
 Costume.destroy_all
+User.destroy_all
 puts "Db is clean"
 
-20.times do |costume|
-  costume = Costume.create(
+
+user = User.create!(
+  email: 'fakeuser@me.com',
+  password: 'secret'
+  )
+
+  20.times do
+    file = URI.open('https://www.reviewjournal.com/wp-content/uploads/2020/10/14359546_web1_halloween.costume-3.jpg?crop=1')
+  costume = Costume.create!(
     name: Faker::Superhero.name,
-    user: User.find(1),
+    user: user,
     category: Faker::Superhero.suffix,
     description: Faker::Superhero.descriptor,
     price: rand(20..99)
-  )
-puts costume.price
+    )
+    costume.photo.attach(io: file, filename: 'diaDeLosMuertos.png', content_type: 'image/png')
 puts "#{costume.name} was create!"
 end
-
+# file.rewind
 puts "#{Costume.count} costumes were created, homeslizzle."
