3b2888b6bbb33deb9839ae015573c2dd75857d31
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-02-25 14:52:05 +0900

    reviews are functional

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_034315) do
+ActiveRecord::Schema.define(version: 2021_02_25_031036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,15 @@ ActiveRecord::Schema.define(version: 2021_02_24_034315) do
     t.index ["user_id"], name: "index_costumes_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.bigint "costume_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["costume_id"], name: "index_reviews_on_costume_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -89,4 +98,5 @@ ActiveRecord::Schema.define(version: 2021_02_24_034315) do
   add_foreign_key "bookings", "costumes"
   add_foreign_key "bookings", "users"
   add_foreign_key "costumes", "users"
+  add_foreign_key "reviews", "costumes"
 end
